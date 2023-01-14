61ad227a550c96408588a3a348ce8c9eef7f4149
Alberto Hitiro Tsugawa <hitiro@live.com>
2023-01-14 01:23:05 -0300

    feat: estilizando pagina de reservas

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,9 +1,32 @@
 import React from 'react';
+import { MdDelete } from 'react-icons/md';
+
+import './style.css'
 
 export default function Reservas() {
   return (
     <div>
-      <h1>Reservas</h1>
+      <h1 className="title" >Voce solicitou 1 reserva</h1>
+
+      <div className="reservas">
+        <img
+          src="https://sujeitoprogramador.com/wp-content/uploads/2019/12/maceio.jpg"
+          alt="maceio"
+        />
+
+        <strong>Viagem Maceio 7 dias</strong>
+        <span>Quatidade: 2</span>
+        <button
+          type="button"
+          onClick={() => { }}
+        >
+          <MdDelete size={20} color="#191919" />
+        </button>
+      </div>
+
+      <footer>
+        <button type="button" >Solicitar reservas</button>
+      </footer>
     </div>
   );
 }
