91a428cd2395a6f230addec5a9586f1d4d24d366
Alberto Hitiro Tsugawa <hitiro@live.com>
2023-01-14 12:03:17 -0300

    feat: atualizando a quantidade

--- src/pages/Reservas/index.js
+++ src/pages/Reservas/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { removeReserve } from '../../store/modules/reserve/actions';
-import { MdDelete } from 'react-icons/md';
+import { removeReserve, updateAmountReserve } from '../../store/modules/reserve/actions';
+import { MdDelete, MdAddCircle, MdRemoveCircle } from 'react-icons/md';
 
 import './style.css'
 
@@ -12,6 +12,14 @@ export default function Reservas() {
 
   function handleRemove(id) {
     dispatch(removeReserve(id));
+  }
+
+  function decrementAmount(trip) {
+    dispatch(updateAmountReserve(trip.id, trip.amount - 1));
+  }
+
+  function incrementAmount(trip) {
+    dispatch(updateAmountReserve(trip.id, trip.amount + 1));
   }
 
 
@@ -28,7 +36,18 @@ export default function Reservas() {
           />
 
           <strong>{reserve.title}</strong>
-          <span>{reserve.amount}</span>
+          <div id="amount">
+            <button type="button" onClick={() => decrementAmount(reserve)}>
+              <MdRemoveCircle size={25} color="#191919" />
+            </button>
+
+            <input type="text" readOnly value={reserve.amount} />
+
+            <button type="button" onClick={() => incrementAmount(reserve)}>
+              <MdAddCircle size={25} color="#191919" />
+            </button>
+          </div>
+
           <button
             type="button"
             onClick={() => handleRemove(reserve.id)}
