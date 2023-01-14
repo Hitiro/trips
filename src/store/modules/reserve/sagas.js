33fe3b652fe233540727edba6639f53988c1f6bd
Alberto Hitiro Tsugawa <hitiro@live.com>
2023-01-14 13:46:54 -0300

    feat: validacao de estoque

--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,5 +1,5 @@
 import { select, call, put, all, takeLatest } from 'redux-saga/effects'
-import { addReserveSuccess, updateAmountReserve } from './actions';
+import { addReserveSuccess, updateAmountSuccess } from './actions';
 import api from '../../../services/api'
 
 function* addToReserve({ id }) {
@@ -8,11 +8,23 @@ function* addToReserve({ id }) {
     state => state.reserve.find(trip => trip.id === id)
   );
 
+  const myStock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = myStock.data.amount;
+
+  const currentSock = tripExists ? tripExists.amount : 0;
+
+  const amount = currentSock + 1;
+
+  if (amount > stockAmount) {
+    alert('quantidade máxima atingida');
+    return;
+  }
+
   if (tripExists) {
     // Aumentar o Amount
-    const amount = tripExists.amount + 1;
 
-    yield put(updateAmountReserve(id, amount));
+    yield put(updateAmountSuccess(id, amount));
 
   } else {
     const response = yield call(api.get, `trips/${id}`);
@@ -26,6 +38,24 @@ function* addToReserve({ id }) {
   }
 }
 
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const myStock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = myStock.data.amount;
+
+  if (amount > stockAmount) {
+    alert('Não há mais estoque disponivel');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+
+
+}
+
 export default all([
-  takeLatest('ADD_RESERVE_REQUEST', addToReserve)
+  takeLatest('ADD_RESERVE_REQUEST', addToReserve),
+  takeLatest('UPDATE_RESERVE_REQUEST', updateAmount),
 ])
